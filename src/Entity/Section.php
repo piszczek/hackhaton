11657c201259bea29f090225595e115f13039597
Andrzej Piszczek <andrzej.piszczek@codibly.com>
2018-11-24 21:54:35 +0100

    add json serializble to sections

--- src/Entity/Section.php
+++ src/Entity/Section.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\SectionRepository")
  */
-class Section
+class Section implements \JsonSerializable
 {
     /**
      * @ORM\Id()
@@ -115,5 +115,23 @@ class Section
         }
 
         return $this;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'name' => $this->getName(),
+            'startPoint' => [
+                'latitude' => $this->getStartPoint()->getLatitude(),
+                'longitude' => $this->getStartPoint()->getLongitude(),
+            ]
+        ];
     }
 }
