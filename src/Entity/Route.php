8ff34a0adec4aa29559510b6e796193b850800aa
Andrzej Piszczek <andrzej.piszczek@codibly.com>
2018-11-25 01:30:55 +0100

    add route

---
+++ src/Entity/Route.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RouteRepository")
+ */
+class Route
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $startAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $endAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Vehicle")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $vehicle;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Section")
+     */
+    private $sections;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isBlocking;
+
+    public function __construct()
+    {
+        $this->sections = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartAt(): ?\DateTimeInterface
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(\DateTimeInterface $startAt): self
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeInterface
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(\DateTimeInterface $endAt): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getVehicle(): ?Vehicle
+    {
+        return $this->vehicle;
+    }
+
+    public function setVehicle(?Vehicle $vehicle): self
+    {
+        $this->vehicle = $vehicle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Section[]
+     */
+    public function getSections(): Collection
+    {
+        return $this->sections;
+    }
+
+    public function addSection(Section $section): self
+    {
+        if (!$this->sections->contains($section)) {
+            $this->sections[] = $section;
+        }
+
+        return $this;
+    }
+
+    public function removeSection(Section $section): self
+    {
+        if ($this->sections->contains($section)) {
+            $this->sections->removeElement($section);
+        }
+
+        return $this;
+    }
+
+    public function getIsBlocking(): ?bool
+    {
+        return $this->isBlocking;
+    }
+
+    public function setIsBlocking(bool $isBlocking): self
+    {
+        $this->isBlocking = $isBlocking;
+
+        return $this;
+    }
+}
